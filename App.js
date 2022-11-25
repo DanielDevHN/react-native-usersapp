6dfd2f2fe54eaac1849708236ab9852e7c3fc399
Daniel Gregorio Reyes Alvarez <daniel.reyes@grupocorinsa.hn>
2022-11-25 08:49:48 -0600

    Adding project files

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import UsersList from "./screens/UsersList";
+import CreateUser from "./screens/CreateUser";
+import UserDetail from "./screens/UserDetail";
+
+const Stack = createNativeStackNavigator();
+
+function MyStack() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="UsersList" component={UsersList} />
+        <Stack.Screen name="CreateUser" component={CreateUser} />
+        <Stack.Screen name="UserDetail" component={UserDetail} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <MyStack />
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
---
+++ screens/UsersList.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const UsersList = () => {
+  return (
+    <View>
+      <Text>Users List</Text>
+    </View>
+  )
+}
+
+export default UsersList
